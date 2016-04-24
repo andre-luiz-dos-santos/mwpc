bd511d6514a694db548753489eec6c88a81de601
André Luiz dos Santos <andre@a.fastville.com.br>
2016-04-24 13:45:28 -0300

    Not needed anymore

--- server/socket.io.d.ts
+++ server/socket.io.d.ts
@@ -1,6 +1,5 @@
 declare namespace SocketIO {
 	interface Socket {
-		on(event: string, listener: Function): Socket;
 		on(event: 'type', listener: (text: string, callback: () => void) => void): Socket;
 	}
 }
