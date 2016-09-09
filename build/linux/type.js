542f04de639d9ef0b8689bc9bcd63f0179aca672
Andre <andre@a.fastville.com.br>
2016-09-09 09:19:10 -0300

    Update built files

--- build/linux/type.js
+++ build/linux/type.js
@@ -1,7 +1,12 @@
 "use strict";
 const child_process_1 = require('child_process');
 const main = function (text) {
-    child_process_1.execFileSync('xdotool', ['type', text]);
+    try {
+        child_process_1.execFileSync('xdotool', text.split(/ /));
+    }
+    catch (err) {
+        process.stderr.write(`xdotool process terminated with error: ${err}\n`);
+    }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = main;
